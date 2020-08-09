87cb44e59cc8863d026073b0a7552c9a11c22ac3
asaki kusano <darumade4@gmail.com>
2020-08-09 16:27:47 +0900

    session crud 制限なし

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'blogs#index'
-
+  
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :blogs do
     post :confirm, on: :collection
